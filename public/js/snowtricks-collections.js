7a5fb943fd40b50eb2d43f3246452b0c8286173f
Riwalenn <riwalenn@gmail.com>
2021-04-13 15:49:52 +0200

    [FIX] codacy

--- public/js/snowtricks-collections.js
+++ public/js/snowtricks-collections.js
@@ -1,6 +1,6 @@
 /*      GESTION DES VIDEOS DANS LA COLLECTION     */
 var videosHolder = $("#videos_list");
-videosHolder.data('index', videosHolder.find(".card").length);
+videosHolder.data("index", videosHolder.find(".card").length);
 
 //ajouter une vidéo
 var addNewVideoBtn = $("<a href='#videos_list' class='btn btn-info' style='margin-top:1rem;'>Ajouter une vidéo</a>");
@@ -29,13 +29,13 @@ videosHolder.find(".card-body").each(function () {
 });
 
 function addNewFormVideo() {
-    var prototype = videosHolder.data('prototype');
-    var index = videosHolder.data('index');
+    var prototype = videosHolder.data("prototype");
+    var index = videosHolder.data("index");
     var newForm = prototype;
 
     newForm = newForm.replace(/__name__/g, index);
 
-    videosHolder.data('index', index++);
+    videosHolder.data("index", index++);
 
     var card = $("<div class='card border-light'></div>");
     var cardBody = $("<div class='card-body alert alert-secondary row'></div>").append(newForm);
